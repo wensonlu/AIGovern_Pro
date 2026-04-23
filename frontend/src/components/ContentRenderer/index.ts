ec5568c8ed31dc575b0369bfeacbd2deda81212c
wclu <wclu@trip.com>
2026-04-23 17:14:37 +0800

    feat: implement ContentRenderer registry and factory pattern

---
+++ frontend/src/components/ContentRenderer/index.ts
@@ -0,0 +1,6 @@
+export { getContentRenderer, registerRenderer, getRegisteredTypes } from './registry';
+export type { ContentType, ContentRendererProps } from './types';
+export { default as TextRenderer } from './TextRenderer';
+export { default as MarkdownRenderer } from './MarkdownRenderer';
+export { default as HtmlRenderer } from './HtmlRenderer';
+export { default as JsonRenderer } from './JsonRenderer';
---
+++ frontend/src/components/ContentRenderer/registry.ts
@@ -0,0 +1,33 @@
+import { FC } from 'react';
+import TextRenderer from './TextRenderer';
+import MarkdownRenderer from './MarkdownRenderer';
+import HtmlRenderer from './HtmlRenderer';
+import JsonRenderer from './JsonRenderer';
+import { ContentRendererProps, ContentType } from './types';
+
+type RendererMap = {
+  [key in ContentType]: FC<ContentRendererProps>;
+};
+
+const renderers: RendererMap = {
+  text: TextRenderer,
+  markdown: MarkdownRenderer,
+  html: HtmlRenderer,
+  json: JsonRenderer,
+};
+
+export const getContentRenderer = (contentType?: string | null): FC<ContentRendererProps> => {
+  const type = contentType as ContentType | undefined;
+  if (type && type in renderers) {
+    return renderers[type];
+  }
+  return renderers.text; // Fallback to text
+};
+
+export const registerRenderer = (type: ContentType, component: FC<ContentRendererProps>) => {
+  renderers[type] = component;
+};
+
+export const getRegisteredTypes = (): ContentType[] => {
+  return Object.keys(renderers) as ContentType[];
+};
